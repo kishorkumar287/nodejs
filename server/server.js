ebe0d37321389a365386b5ca547d38fc9aa3829e
kishorkumar287 <60978027+kishorkumar287@users.noreply.github.com>
2020-04-26 09:08:06 +0530

    simple response page

--- server/server.js
+++ server/server.js
@@ -4,4 +4,16 @@ const app = express();
 console.log(typeof express);
 console.log(typeof app);
 
-app.listen(3100,console.log("kishor is back...."));
+
+app.get("/",(req,res)=>{
+    res.json("hello this is kishor...........");
+
+
+}
+);
+
+
+app.listen(3100, () => {
+    console.log("API server started in port 3100!");
+
+});
